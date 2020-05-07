2494a36fcd039dffddb543c397ab6fb0d1729eff
CrowdYellow <echo61546@gmail.com>
2020-05-07 13:53:59 +0800

    v1.0.4 添加注释

--- api/hzj/controller/DropListController.php
+++ api/hzj/controller/DropListController.php
@@ -11,10 +11,11 @@ class DropListController extends RestBaseController
 {
     public function index($value)
     {
+        # 根据参数获取对应的下拉菜单，没有匹配的返回空
         if (method_exists($this, $value)) {
-            return $this->success('请求成功！', ['dropList' => $this->$value()]);
+            $this->success('请求成功！', ['dropList' => $this->$value()]);
         }
-        return $this->success('请求成功！', []);
+        $this->error('暂无数据！');
     }
 
     public function users()
