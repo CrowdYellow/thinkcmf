4c6468456de6583394a3772a348999e5e4237705
CrowdYellow <echo61546@gmail.com>
2020-05-09 17:08:35 +0800

    v1.0.34 添加categories和information的swagger

--- api/hzj/controller/CategoriesController.php
+++ api/hzj/controller/CategoriesController.php
@@ -9,6 +9,28 @@ use cmf\controller\RestBaseController;
 
 class CategoriesController extends RestBaseController
 {
+    /**
+     * @OA\Get(
+     *     tags={"新闻"},
+     *     path="/api/hzj/categories",
+     *     operationId="api.categories",
+     *     summary="分类列表",
+     *     description="分类列表",
+     *     @OA\Response(
+     *          response="200",
+     *          description="请求失败",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(
+     *                  @OA\Property(property="code", type="integer", description="响应code"),
+     *                  @OA\Property(property="msg", type="string", description="响应消息"),
+     *                  @OA\Property(property="data", type="array", description="响应参数", @OA\Items()
+     *                  ),
+     *              ),
+     *          ),
+     *       )
+     * )
+     */
     public function index()
     {
         $categories = Category::all();
--- api/hzj/controller/InformationController.php
+++ api/hzj/controller/InformationController.php
@@ -1,14 +1,44 @@
 <?php
 
-
 namespace api\hzj\controller;
-
 
 use app\api\model\Information;
 use cmf\controller\RestBaseController;
 
 class InformationController extends RestBaseController
 {
+    /**
+     * @OA\Get(
+     *     tags={"新闻"},
+     *     path="/api/hzj/information",
+     *     operationId="api.information",
+     *     summary="新闻",
+     *     description="新闻",
+     *     @OA\Parameter(name="type", required=true, in="query", description="类型", @OA\Schema(type="string")),
+     *     @OA\Response(
+     *          response="200",
+     *          description="请求失败",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(
+     *                  @OA\Property(property="code", type="integer", description="响应code"),
+     *                  @OA\Property(property="msg", type="string", description="响应消息"),
+     *                  @OA\Property(property="data", type="array", description="响应参数", @OA\Items(
+     *                          @OA\Property(property="id", type="integer", description="ID"),
+     *                          @OA\Property(property="title", type="string", description="标题"),
+     *                          @OA\Property(property="body", type="string", description="内容"),
+     *                          @OA\Property(property="type", type="integer", description="类型:1->党建动态, 2->市场信息， 3->活动预告"),
+     *                          @OA\Property(property="cover_pic", type="string", description="封面图"),
+     *                          @OA\Property(property="create_time", type="string", description="创建时间"),
+     *                          @OA\Property(property="update_time", type="string", description="修改时间"),
+     *
+     *                      )
+     *                  ),
+     *              ),
+     *          ),
+     *       )
+     * )
+     */
     public function index()
     {
         $information = Information::where('type', input('type'))->paginate(input('page'));
